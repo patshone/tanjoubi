94d64a1cf8c595f6f7e84ea6e096571a23726564
Patrick Shone <patshone@gmail.com>
2011-04-26 16:06:24 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanjoubi::Application.config.secret_token = 'f63a3bfa89e8c6ab6ce8822a7280b44611b9f9b2daafa4d1aff142fa8e875a4dcde5bcda4ca767b1635e7af058061d323334fb54f92d27725a85c9e092951b2d'
